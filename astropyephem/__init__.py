8928628e8f34942d41532c2b4fbddc36de6997b8
Alex Rudy <alex.rudy@gmail.com>
2014-03-15 17:14:40 -0700

    Removing configuration warnings for astropyephem.

--- astropyephem/__init__.py
+++ astropyephem/__init__.py
@@ -110,24 +110,3 @@ def test(package=None, test_path=None, args=None, plugins=None,
         plugins=plugins, verbose=verbose, pastebin=pastebin,
         remote_data=remote_data, pep8=pep8, pdb=pdb,
         coverage=coverage, open_files=open_files, **kwargs)
-
-if not _ASTROPY_SETUP_:
-
-    import os
-    from warnings import warn
-    from astropy import config
-
-    # add these here so we only need to cleanup the namespace at the end
-    config_dir = None
-
-    if not os.environ.get('ASTROPY_SKIP_CONFIG_UPDATE', False):
-        config_dir = os.path.dirname(__file__)
-        try:
-            config.configuration.update_default_config(__package__, config_dir)
-        except config.configuration.ConfigurationDefaultMissingError as e:
-            wmsg = (e.args[0] + " Cannot install default profile. If you are "
-                    "importing from source, this is expected.")
-            warn(config.configuration.ConfigurationDefaultMissingWarning(wmsg))
-            del e
-
-    del os, warn, config_dir  # clean up namespace
