e5d8be7fc9ebae90acb601eb041cf0968ff58b73
Alex Rudy <alex.rudy@gmail.com>
2014-03-15 17:14:17 -0700

    Moving astropyephem tests into place.

---
+++ astropyephem/tests/setup_package.py
@@ -0,0 +1,3 @@
+def get_package_data():
+    return {
+        'astropyephem.tests': ['coveragerc']}
--- packagename/tests/setup_package.py
+++
@@ -1,3 +0,0 @@
-def get_package_data():
-    return {
-        'packagename.tests': ['coveragerc']}
